77635e201a20c44ca309c3b91ab944a379383c32
naldurien <nicole.aldurien@gmail.com>
2021-05-07 10:29:39 -0400

    adjusting description length on homepage

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -7,7 +7,7 @@ const Product = ({imageUrl, name, price, description, productId}) => {
             <img src= {imageUrl} alt= {name}/>
             <div className = 'product__info'>
                 <p className = 'info__name'>{name}</p>
-                <p className = 'info__description'>{description.substring(0, 100)}...</p>
+                <p className = 'info__description'>{description.substring(0, 117)}...</p>
                 <p className = 'info__price'>${price}</p>
                 <Link to = {`/product/${productId}`} className = 'info__button'>View</Link>
             </div>
